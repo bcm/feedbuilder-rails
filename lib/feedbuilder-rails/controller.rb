7fad7381b3a07e9bee3a0888c650799923b699df
Brian Moseley <bcm@maz.org>
2010-12-22 10:33:37 -0500

    little fixups to get it working for reals

--- lib/feedbuilder-rails/controller.rb
+++ lib/feedbuilder-rails/controller.rb
@@ -2,7 +2,7 @@ module FeedBuilder
   module Rails
     module Controller
       def send_feed(filename, options = {}, &block)
-        if Rails.env == 'development' || stale?(options)
+        if ::Rails.env == 'development' || stale?(options)
           feed ||= yield if block_given?
           send_data(feed.to_xml, :filename => filename, :type => :atom, :disposition => 'inline')
         end
--- lib/feedbuilder-rails/provider.rb
+++ lib/feedbuilder-rails/provider.rb
@@ -1,9 +1,5 @@
 class ActiveRecord::Base
   def self.acts_as_feed_provider(options = {})
-    unless (options.include?(:feed_id_domain) && options.include?(:feed_id_path)) || options.include?(:feed_id)
-      raise ArgumentError, 'Must include either :feed_id_domain and :feed_id_path or :feed_id'
-    end
-
     extend FeedBuilder::Provider
 
     self.feed_id_domain = options[:feed_id_domain]
